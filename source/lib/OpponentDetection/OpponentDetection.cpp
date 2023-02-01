c7122e717e18d91dedf1c24dba347862c35298c8
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-02-01 16:53:40 +0100

    fixed lidar wrapper

--- source/lib/OpponentDetection/OpponentDetection.cpp
+++ source/lib/OpponentDetection/OpponentDetection.cpp
@@ -7,41 +7,40 @@ private:
 	int MotorPin = 26;
 	int MotorSpeed = 150;
 	RPLidar lidar;
-
-public:
-	void begin(int motor_pin, int motor_speed, HardwareSerial serial)
+	void LidarReconnect()
 	{
-		
-		this->MotorPin = motor_pin;
-		this->MotorSpeed = motor_speed;
-		this->lidar.begin(serial);
-	}
-};
-
-void setLidarSpeed(int NewSpeed)
-{
-	LIDAR_SPEED = NewSpeed;
-}
-
-RPLidarMeasurement GetLidarPoint()
-{
-	RPLidarMeasurement Point;
-	if (IS_OK(lidar.waitPoint()))
-	{
-		Point = lidar.getCurrentPoint();
-	}
-	else
-	{
-		analogWrite(LIDAR_MOTOR, 0);
+		analogWrite(this->MotorPin, 0);
 		delay(15);
 		rplidar_response_device_info_t info;
 		if (IS_OK(lidar.getDeviceInfo(info, 100)))
 		{
 			Serial.println("Lidar Detected...");
 			lidar.startScan();
-			analogWrite(LIDAR_MOTOR, LIDAR_SPEED);
-			delay(1000);
+			analogWrite(this->MotorPin, this->MotorSpeed);
+			delay(500);
 		}
 	}
-	return Point;
-}
+
+public:
+	void begin(int motor_pin, int motor_speed, HardwareSerial serial)
+	{
+
+		this->MotorPin = motor_pin;
+		this->MotorSpeed = motor_speed;
+		this->lidar.begin(serial);
+	}
+
+	RPLidarMeasurement GetLidarPoint()
+	{
+		RPLidarMeasurement Point;
+		if (IS_OK(lidar.waitPoint()))
+		{
+			Point = lidar.getCurrentPoint();
+		}
+		else
+		{
+			LidarReconnect();
+		}
+		return Point;
+	}
+};
