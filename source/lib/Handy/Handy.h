c8160d77b5ac47f6b50705b0f162c5a5b4f1eb3e
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-02-02 17:46:11 +0100

    added handy Header

---
+++ source/lib/Handy/Handy.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include <uri/UriBraces.h>
+class Handy
+{
+private:
+	WebServer server;
+	// Access Point Settings
+	const char *AP_SSID = "HandyMan"; // Access point name
+	const char *AP_PWD = "HANDSOME";  // Access point Password
+	// Steppers Settings & Pins
+	const int PIN_Dir_Motor_1 = 33;
+	const int PIN_Stp_Motor_1 = 25;
+	const int PIN_Dir_Motor_2 = 26;
+	const int PIN_Stp_Motor_2 = 27;
+	const int PIN_Dir_Motor_3 = 12;
+	const int PIN_Stp_Motor_3 = 14;
+	const int PIN_Enable = 13;
+	AccelStepper Motor_1;
+	AccelStepper Motor_2;
+	AccelStepper Motor_3;
+	MultiStepper Motors;
+	// Endpoints
+	UriBraces FLOW_URI = UriBraces("/flow/{}/{}/{}");
+	UriBraces STEPS_URI = UriBraces("/steps/{}/{}/{}");
+	void InitMotors();
+	void InitRoutes();
+	void Flow_Controller();
+	void Steps_Controller();
+
+public:
+	Handy();
+	void setup(void);
+	void listen();
+};
--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,54 +1,54 @@
-#include <Arduino.h>
-#include <RPLidar.h>
-#define RPLIDAR_MOTOR 26
-#define LED_BLUE 27
-#define TABLE_HEIGHT 10000 // in mm
-#define TABLE_WIDTH 10000  // in mm
-RPLidar lidar;
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(RPLIDAR_MOTOR, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  lidar.begin(Serial2);
-}
+// #include <Arduino.h>
+// #include <RPLidar.h>
+// #define RPLIDAR_MOTOR 26
+// #define LED_BLUE 27
+// #define TABLE_HEIGHT 10000 // in mm
+// #define TABLE_WIDTH 10000  // in mm
+// RPLidar lidar;
+// void setup()
+// {
+//   Serial.begin(115200);
+//   pinMode(RPLIDAR_MOTOR, OUTPUT);
+//   pinMode(LED_BLUE, OUTPUT);
+//   lidar.begin(Serial2);
+// }
 
-void loop()
-{
-  int target_radius = 30;
-  int target_angle = 210;
-  int min_angle = target_angle - (target_radius / 2);
-  int max_angle = target_angle + (target_radius / 2);
+// void loop()
+// {
+//   int target_radius = 30;
+//   int target_angle = 210;
+//   int min_angle = target_angle - (target_radius / 2);
+//   int max_angle = target_angle + (target_radius / 2);
 
-  if (IS_OK(lidar.waitPoint()))
-  {
-    float distance = lidar.getCurrentPoint().distance; // distance value in mm unit
-    float angle = lidar.getCurrentPoint().angle;       // angle value in degree
-    bool startBit = lidar.getCurrentPoint().startBit;  // whether this point is belong to a new scan
-    byte quality = lidar.getCurrentPoint().quality;    // quality of the current measurement
-    if ((angle >= min_angle) && (angle <= max_angle) && (distance > 0))
-    {
-      if (distance < 1000)
-      {
-        digitalWrite(LED_BLUE, HIGH);
-      }
-      else
-      {
-        digitalWrite(LED_BLUE, LOW);
-      }
-    }
-  }
-  else
-  {
-    analogWrite(RPLIDAR_MOTOR, 0);
-    delay(15);
-    rplidar_response_device_info_t info;
-    if (IS_OK(lidar.getDeviceInfo(info, 100)))
-    {
-      Serial.println("Lidar Detected...");
-      lidar.startScan();
-      analogWrite(RPLIDAR_MOTOR, 150);
-      delay(1000);
-    }
-  }
-}
+//   if (IS_OK(lidar.waitPoint()))
+//   {
+//     float distance = lidar.getCurrentPoint().distance; // distance value in mm unit
+//     float angle = lidar.getCurrentPoint().angle;       // angle value in degree
+//     bool startBit = lidar.getCurrentPoint().startBit;  // whether this point is belong to a new scan
+//     byte quality = lidar.getCurrentPoint().quality;    // quality of the current measurement
+//     if ((angle >= min_angle) && (angle <= max_angle) && (distance > 0))
+//     {
+//       if (distance < 1000)
+//       {
+//         digitalWrite(LED_BLUE, HIGH);
+//       }
+//       else
+//       {
+//         digitalWrite(LED_BLUE, LOW);
+//       }
+//     }
+//   }
+//   else
+//   {
+//     analogWrite(RPLIDAR_MOTOR, 0);
+//     delay(15);
+//     rplidar_response_device_info_t info;
+//     if (IS_OK(lidar.getDeviceInfo(info, 100)))
+//     {
+//       Serial.println("Lidar Detected...");
+//       lidar.startScan();
+//       analogWrite(RPLIDAR_MOTOR, 150);
+//       delay(1000);
+//     }
+//   }
+// }
